4b15dd2a9012eeacc52823a4411f2dd0ccaee147
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2025-04-14 10:09:21 +0530

    Install MUI Package and Explore it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
-
+import Button from '@mui/material/Button';
+import Navbar from "./Navbar";
 function App() {
   return (
     <>
+    <Navbar />
       Welcome
+      <div>
+        <Button variant="contained">Hello world</Button>
+      </div>
+
     </>
   )
 }
